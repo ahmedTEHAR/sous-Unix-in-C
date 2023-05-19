5712599ee88dfad966d1afa738713c12fe919cd3
Mahdjoubabderraouf <95053965+Mahdjoubabderraouf@users.noreply.github.com>
2023-05-19 23:23:45 +0100

    fun

--- wait.c
+++ wait.c
@@ -7,7 +7,7 @@
 int main () {
     
 pid_t pid, id ; 
-int *status;
+int status;
 if ((pid=fork())==0) {
         printf ("Je suis le fils avec pid %d\n" , getpid ());
         sleep(10);
@@ -16,10 +16,11 @@ if ((pid=fork())==0) {
 else if ( pid > 0) {
         printf ("Je suis le pere avec pid %d\n" , getpid ());
         printf (" J’ attends que mon fils se termine\n" );
-    While((id=waitpid(pid,&status,WNOHANG))==0)
-        printf(" mon fils n’ a pas encore terminé son exécution");
+    while((id=waitpid(pid,&status,WNOHANG))==0){
+        printf(" mon fils n a pas encore terminé son exécution");}
         if(WIFEXITED(status)) printf("fils %d terminé par exit(%d)\n",id,WEXITSTATUS(status)) ;
 }
+
 else {
     printf ("Erreur dans la creation du fils\n" );
     exit (2);
